714dfa663da90a7f24e404f4a4778a9cdb45e4be
Satyam0327 <satyamsingh0327@gmail.com>
2023-11-01 19:29:49 +0530

    bro

---
+++ src/pageInfo.js
@@ -0,0 +1,55 @@
+import firstPage from './images/image-first.jpg';
+import secondPage from './images/image-second.jpg';
+import thirdPage from './images/image-third.jpg';
+import fourthPage from './images/image-fourth.jpg';
+import fifthPage from './images/image-fifth.jpg';
+import sixthPage from './images/image-sixth.jpg';
+import seventhPage from './images/image-seventh.jpg';
+import eighthPage from './images/image-eighth.jpg';
+
+const pages = [
+    {
+        id: 1,
+        "image": firstPage,
+        desc: "JavaScript is a scripting or programming language that allows you to implement complex features on web pages — every time a web page does more than just sit there and display static information for you to look at — displaying timely content updates, interactive maps, animated 2D/3D graphics, scrolling video jukeboxes, etc. — you can bet that JavaScript is probably involved. It is the third layer of the layer cake of standard web technologies, two of which (HTML and CSS) we have covered in much more detail in other parts of the Learning Area."
+    },
+    {
+        id: 2,
+        image: secondPage,
+        desc: "JavaScript is a scripting or programming language that allows you to implement complex features on web pages — every time a web page does more than just sit there and display static information for you to look at — displaying timely content updates, interactive maps, animated 2D/3D graphics, scrolling video jukeboxes, etc. — you can bet that JavaScript is probably involved. It is the third layer of the layer cake of standard web technologies, two of which (HTML and CSS) we have covered in much more detail in other parts of the Learning Area."
+
+    },
+    {
+        id: 3,
+        image: thirdPage,
+        desc: "JavaScript is a scripting or programming language that allows you to implement complex features on web pages — every time a web page does more than just sit there and display static information for you to look at — displaying timely content updates, interactive maps, animated 2D/3D graphics, scrolling video jukeboxes, etc. — you can bet that JavaScript is probably involved. It is the third layer of the layer cake of standard web technologies, two of which (HTML and CSS) we have covered in much more detail in other parts of the Learning Area."
+
+    },
+    {
+        id: 4,
+        image: fourthPage,
+        desc: "JavaScript is a scripting or programming language that allows you to implement complex features on web pages — every time a web page does more than just sit there and display static information for you to look at — displaying timely content updates, interactive maps, animated 2D/3D graphics, scrolling video jukeboxes, etc. — you can bet that JavaScript is probably involved. It is the third layer of the layer cake of standard web technologies, two of which (HTML and CSS) we have covered in much more detail in other parts of the Learning Area."
+    },
+    {
+        id: 5,
+        image: fifthPage,
+        desc: "JavaScript is a scripting or programming language that allows you to implement complex features on web pages — every time a web page does more than just sit there and display static information for you to look at — displaying timely content updates, interactive maps, animated 2D/3D graphics, scrolling video jukeboxes, etc. — you can bet that JavaScript is probably involved. It is the third layer of the layer cake of standard web technologies, two of which (HTML and CSS) we have covered in much more detail in other parts of the Learning Area."
+    },
+    {
+        id: 6,
+        image: sixthPage,
+        desc: "В тот день, когда отчаяние начало брать верх, впереди замаячил свет костра в непроходимых джунглях. Это казалось чудом. Им оно и было."
+    },
+    {
+        id: 7,
+        image: seventhPage,
+        desc: "JavaScript is a scripting or programming language that allows you to implement complex features on web pages — every time a web page does more than just sit there and display static information for you to look at — displaying timely content updates, interactive maps, animated 2D/3D graphics, scrolling video jukeboxes, etc. — you can bet that JavaScript is probably involved. It is the third layer of the layer cake of standard web technologies, two of which (HTML and CSS) we have covered in much more detail in other parts of the Learning Area."
+    },
+    {
+        id: 8,
+        image: eighthPage,
+        desc: "JavaScript is a scripting or programming language that allows you to implement complex features on web pages — every time a web page does more than just sit there and display static information for you to look at — displaying timely content updates, interactive maps, animated 2D/3D graphics, scrolling video jukeboxes, etc. — you can bet that JavaScript is probably involved. It is the third layer of the layer cake of standard web technologies, two of which (HTML and CSS) we have covered in much more detail in other parts of the Learning Area."
+    }
+];
+
+export default pages
